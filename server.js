32b8eb43c9d765c97a1b31a94a24947650a34360
mpeng <michaelxpeng@gmail.com>
2018-08-11 12:26:49 -0400

    functional

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ var PORT = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// var waitingList = [];
-
 // Starts the server to begin listening
 // =============================================================
 
+var tables = [{
+  name: "Rommel",
+  phone: "1234567890",
+  email: "rommel@cavs.com",
+  id: 23,
+}];
+
   // Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
 app.get("/view", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
@@ -44,48 +53,11 @@ app.post("/api/tables", function(req, res) {
 
   console.log(newtable);
 
-  table.push(newtable);
+  tables.push(newtable);
 
   res.json(newtable);
 });
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-
-  // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
-
-// Displays all tables
-app.get("/api/tables", function(req, res) {
-  return res.json(tables);
-});
-
-// Create New Tables - takes in JSON input
-app.post("/api/tables", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
-  var newtable = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newtable);
-
-  table.push(newtable);
-
-  res.json(newtable);
-});
-
-// Starts the server to begin listening
   console.log("App listening on PORT " + PORT);
 });
