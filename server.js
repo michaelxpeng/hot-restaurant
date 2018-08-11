ff3201265a39fdd9ebbb051ddf02e0ebe5f1e37e
George Rossakis <george@MacBook-Pro-2.local>
2018-08-11 11:39:47 -0400

    view html

--- server.js
+++ server.js
@@ -13,11 +13,52 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var guests = [];
-var waitingList = [];
+// var guests = [{name: "yoda",
+// phone: "Yoda",
+// email: "Jedi Master",
+// id: 900,
+// }];
+// var waitingList = [];
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+  // Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all tables
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+
+// Create New Tables - takes in JSON input
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newtable = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newtable);
+
+  table.push(newtable);
+
+  res.json(newtable);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
