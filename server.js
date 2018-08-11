75c235eae223180b6cc31c858fece2102f2f64e2
Crystal Espaillat <crystalespaillat@Crystals-MacBook-Pro.local>
2018-08-11 11:41:12 -0400

    Fixed listening error

--- server.js
+++ server.js
@@ -7,27 +7,24 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// var guests = [{name: "yoda",
-// phone: "Yoda",
-// email: "Jedi Master",
-// id: 900,
+// var tables = [{name: "yoda",
+//   phone: "Yoda",
+//   email: "Jedi Master",
+//   id: 900,
 // }];
 // var waitingList = [];
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
 
   // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/view", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
